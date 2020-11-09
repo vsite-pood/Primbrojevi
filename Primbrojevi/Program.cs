31552861a762f43689d18290b3b3251ff00f6e22
Mislav <mislav.franja@gmail.com>
2020-11-09 18:48:32 +0100

    Split algorithm in 3 methods

--- Primbrojevi/Program.cs
+++ Primbrojevi/Program.cs
@@ -29,15 +29,60 @@ namespace Vsite.Pood
             }
         }
 
+        static int s;
+        static bool[] f;
+        static int[] primovi;
+
         // Primjer iz knjige  Robert C. Martin: "Agile Software Development"!!!
         public static int[] GenerirajPrimBrojeve(int max)
         {
             if (max < 2)
                 return new int[0]; // vrati prazan niz
-                 
-            // deklaracije
-            int s = max + 1; // duljina niza
-            bool[] f = new bool[s]; // niz s primbrojevima
+
+            NapraviSito(max);
+            Prosijaj();
+            return PokupiPrimove();
+
+        }
+
+        private static int[] PokupiPrimove()
+        {
+            // koliko je primbrojeva?
+            int broj = 0;
+            for (int i = 0; i < s; ++i)
+            {
+                if (f[i])
+                    ++broj;
+            }
+
+            primovi = new int[broj];
+
+            // prebaci primbrojeve u rezultat
+            for (int i = 0, j = 0; i < s; ++i)
+            {
+                if (f[i])
+                    primovi[j++] = i;
+            }
+            return primovi; // vrati niz brojeva
+        }
+
+        private static void Prosijaj()
+        {
+            // sito (ide do kvadratnog korijena maksimalnog broja)
+            for (int i = 2; i < Math.Sqrt(s) + 1; ++i)
+            {
+                if (f[i]) // ako je i prekrižen, prekriži i višekratnike
+                {
+                    for (int j = 2 * i; j < s; j += i)
+                        f[j] = false; // višekratnik nije primbroj
+                }
+            }
+        }
+
+        private static void NapraviSito(int max)
+        {
+            s = max + 1; // duljina niza
+            f = new bool[s]; // niz s primbrojevima
             int i;
 
             // inicijaliziramo sve na true
@@ -46,36 +91,6 @@ namespace Vsite.Pood
 
             // ukloni 0 i 1 koji su primbrojevi po definiciji
             f[0] = f[1] = false;
-
-            // sito (ide do kvadratnog korijena maksimalnog broja)
-            int j;
-            for (i = 2; i < Math.Sqrt(s) + 1; ++i)
-            {
-                if (f[i]) // ako je i prekrižen, prekriži i višekratnike
-                {
-                    for (j = 2 * i; j < s; j += i)
-                        f[j] = false; // višekratnik nije primbroj
-                }
-            }
-
-            // koliko je primbrojeva?
-            int broj = 0;
-            for (i = 0; i < s; ++i)
-            {
-                if (f[i])
-                    ++broj;
-            }
-
-            int[] primovi = new int[broj];
-
-            // prebaci primbrojeve u rezultat
-            for (i = 0, j = 0; i < s; ++i)
-            {
-                if (f[i])
-                    primovi[j++] = i;
-            }
-            return primovi; // vrati niz brojeva
-           
         }
     }
 }
