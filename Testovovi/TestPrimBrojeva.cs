43f7e96a3df0196a30b09bbbd6d0f4f05e9d9d08
the.pero.forever@gmail.com <the.pero.forever@gmail.com>
2020-11-09 18:37:03 +0100

    Inversed if deleted else

--- Testovovi/TestPrimBrojeva.cs
+++ Testovovi/TestPrimBrojeva.cs
@@ -12,27 +12,32 @@ namespace Testovovi
         {
             Assert.AreEqual(0, Program.GenerirajPrimBrojeve(0).Length);
         }
+
         [TestMethod]
         public void GenerirajPrimBrojeveZa1VracaPrazanNiz()
         {
             Assert.AreEqual(0, Program.GenerirajPrimBrojeve(1).Length);
         }
+
         [TestMethod]
         public void GenerirajPrimBrojeveZa2VracaBroj2()
         {
             Assert.AreEqual(1, Program.GenerirajPrimBrojeve(2).Length);
             Assert.AreEqual(2, Program.GenerirajPrimBrojeve(2)[0]);
         }
+
         [TestMethod]
         public void GenerirajPrimBrojeveZa100Vraca25Brojeva()
         {
             Assert.AreEqual(25, Program.GenerirajPrimBrojeve(100).Length);
         }
+
         [TestMethod]
         public void GenerirajPrimBrojeveZa100Vraca7naTrecojPoziciji()
         {
             Assert.AreEqual(7, Program.GenerirajPrimBrojeve(100)[3]);
         }
+
         [TestMethod]
         public void GenerirajPrimBrojeveZa100Vraca97naZadnjojPoziciji()
         {
