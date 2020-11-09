92f1c5f288029471dc6d4c8dae59d72fc1ac5d59
Marin <angelo@MYPC>
2020-11-09 19:45:01 +0100

    Split algortihm into three methods

--- Primbrojevi/Program.cs
+++ Primbrojevi/Program.cs
@@ -38,48 +38,63 @@ namespace Vsite.Pood
         {
             if (max < 2)
                 return new int[0]; // vrati prazan niz
-            
-                // deklaracije
-                s = max + 1; // duljina niza
-                f = new bool[s]; // niz s primbrojevima
-                int i;
 
-                // inicijaliziramo sve na true
-                for (i = 0; i < s; ++i)
-                    f[i] = true;
+            // deklaracije
+            KreirajSito(max);
 
-                // ukloni 0 i 1 koji su primbrojevi po definiciji
-                f[0] = f[1] = false;
+            // sito (ide do kvadratnog korijena maksimalnog broja)
 
-                // sito (ide do kvadratnog korijena maksimalnog broja)
-                int j;
-                for (i = 2; i < Math.Sqrt(s) + 1; ++i)
+            Prosijaj();
+
+            // koliko je primbrojeva?
+            return PokupiPrimove();
+
+        }
+
+        private static int[] PokupiPrimove()
+        {
+            int broj = 0;
+            for (int i = 0; i < s; ++i)
+            {
+                if (f[i])
+                    ++broj;
+            }
+
+            primovi = new int[broj];
+
+            // prebaci primbrojeve u rezultat
+            for (int i = 0, j = 0; i < s; ++i)
+            {
+                if (f[i])
+                    primovi[j++] = i;
+            }
+            return primovi; // vrati niz brojeva
+        }
+
+        private static void Prosijaj()
+        {
+            for (int i = 2; i < Math.Sqrt(s) + 1; ++i)
+            {
+                if (f[i]) // ako je i prekrižen, prekriži i višekratnike
                 {
-                    if (f[i]) // ako je i prekrižen, prekriži i višekratnike
-                    {
-                        for (j = 2 * i; j < s; j += i)
-                            f[j] = false; // višekratnik nije primbroj
-                    }
+                    for (int j = 2 * i; j < s; j += i)
+                        f[j] = false; // višekratnik nije primbroj
                 }
+            }
+        }
 
-                // koliko je primbrojeva?
-                int broj = 0;
-                for (i = 0; i < s; ++i)
-                {
-                    if (f[i])
-                        ++broj;
-                }
+        private static void KreirajSito(int max)
+        {
+            s = max + 1; // duljina niza
+            f = new bool[s]; // niz s primbrojevima
 
-                primovi = new int[broj];
 
-                // prebaci primbrojeve u rezultat
-                for (i = 0, j = 0; i < s; ++i)
-                {
-                    if (f[i])
-                        primovi[j++] = i;
-                }
-                return primovi; // vrati niz brojeva
-            
+            // inicijaliziramo sve na true
+            for (int i = 0; i < s; ++i)
+                f[i] = true;
+
+            // ukloni 0 i 1 koji su primbrojevi po definiciji
+            f[0] = f[1] = false;
         }
     }
 }
