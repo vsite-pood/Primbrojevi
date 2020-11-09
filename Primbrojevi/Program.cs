703059310129fe45eac6c2820dba5094f73b6c0a
Ante Mišković <ante.miskovic@adnet.hr>
2020-11-09 18:36:44 +0100

    Inverse if-else blocks

--- Primbrojevi/Program.cs
+++ Primbrojevi/Program.cs
@@ -32,51 +32,50 @@ namespace Vsite.Pood
         // Primjer iz knjige  Robert C. Martin: "Agile Software Development"!!!
         public static int[] GenerirajPrimBrojeve(int max)
         {
-            if (max >= 2)
-            {
-                // deklaracije
-                int s = max + 1; // duljina niza
-                bool[] f = new bool[s]; // niz s primbrojevima
-                int i;
+			if ( max < 2 )
+				return new int[0]; // vrati prazan niz
 
-                // inicijaliziramo sve na true
-                for (i = 0; i < s; ++i)
-                    f[i] = true;
+			// deklaracije
+			int s = max + 1; // duljina niza
+			bool[] f = new bool[s]; // niz s primbrojevima
+			int i;
 
-                // ukloni 0 i 1 koji su primbrojevi po definiciji
-                f[0] = f[1] = false;
+			// inicijaliziramo sve na true
+			for ( i = 0; i < s; ++i )
+				f[i] = true;
 
-                // sito (ide do kvadratnog korijena maksimalnog broja)
-                int j;
-                for (i = 2; i < Math.Sqrt(s) + 1; ++i)
-                {
-                    if (f[i]) // ako je i prekrižen, prekriži i višekratnike
-                    {
-                        for (j = 2 * i; j < s; j += i)
-                            f[j] = false; // višekratnik nije primbroj
-                    }
-                }
+			// ukloni 0 i 1 koji su primbrojevi po definiciji
+			f[0] = f[1] = false;
 
-                // koliko je primbrojeva?
-                int broj = 0;
-                for (i = 0; i < s; ++i)
-                {
-                    if (f[i])
-                        ++broj;
-                }
+			// sito (ide do kvadratnog korijena maksimalnog broja)
+			int j;
+			for ( i = 2; i < Math.Sqrt( s ) + 1; ++i )
+			{
+				if ( f[i] ) // ako je i prekrižen, prekriži i višekratnike
+				{
+					for ( j = 2 * i; j < s; j += i )
+						f[j] = false; // višekratnik nije primbroj
+				}
+			}
 
-                int[] primovi = new int[broj];
+			// koliko je primbrojeva?
+			int broj = 0;
+			for ( i = 0; i < s; ++i )
+			{
+				if ( f[i] )
+					++broj;
+			}
 
-                // prebaci primbrojeve u rezultat
-                for (i = 0, j = 0; i < s; ++i)
-                {
-                    if (f[i])
-                        primovi[j++] = i;
-                }
-                return primovi; // vrati niz brojeva
-            }
-            else
-                return new int[0]; // vrati prazan niz
-        }
+			int[] primovi = new int[broj];
+
+			// prebaci primbrojeve u rezultat
+			for ( i = 0, j = 0; i < s; ++i )
+			{
+				if ( f[i] )
+					primovi[j++] = i;
+			}
+			return primovi; // vrati niz brojeva
+			
+		}
     }
 }
