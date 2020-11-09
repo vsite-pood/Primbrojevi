7f15316eafb2b064f16d97d4e0a31e2183394aaa
Mislav <mislav.franja@gmail.com>
2020-11-09 18:25:48 +0100

    unit tests

---
+++ Testovi/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testovi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testovi")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("58ccbf74-4534-4932-ae8c-dbeabee55ace")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Testovi/TestPrimbrojeva.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Pood;
+
+namespace Testovi
+{
+    [TestClass]
+    public class TestPrimbrojeva
+    {
+        [TestMethod]
+        public void GenerirajPrimBrojeveZa0VracaPrazanNiz()
+        {
+            Assert.AreEqual(0, Program.GenerirajPrimBrojeve(0).Length);
+        }
+
+        [TestMethod]
+        public void GenerirajPrimBrojeveZa1VracaPrazanNiz()
+        {
+            Assert.AreEqual(0, Program.GenerirajPrimBrojeve(1).Length);
+        }
+
+        [TestMethod]
+        public void GenerirajPrimBrojeveZa2VracaBroj2()
+        {
+            Assert.AreEqual(1, Program.GenerirajPrimBrojeve(2).Length);
+            Assert.AreEqual(2, Program.GenerirajPrimBrojeve(2)[0]);
+        }
+
+        [TestMethod]
+        public void GenerirajPrimBrojeveZa100Vraca25PrimBrojeva()
+        {
+            Assert.AreEqual(25, Program.GenerirajPrimBrojeve(100).Length);         
+        }
+
+        [TestMethod]
+        public void GenerirajPrimBrojeveZa100VracaprimBroj7()
+        {
+            Assert.AreEqual(7, Program.GenerirajPrimBrojeve(100)[3]);
+        }
+        [TestMethod]
+        public void GenerirajPrimBrojeveZa100VracaprimBroj97()
+        {
+            Assert.AreEqual(97, Program.GenerirajPrimBrojeve(100)[24]);
+        }
+    }
+}
