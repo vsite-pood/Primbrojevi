16a053ae38dae4a5fd8f3f1f6377b2199e555bf0
Ante Mišković <ante.miskovic@adnet.hr>
2020-11-09 18:53:31 +0100

    rename methods

--- Primbrojevi/Program.cs
+++ Primbrojevi/Program.cs
@@ -38,9 +38,9 @@ namespace Vsite.Pood
 			if ( max < 2 )
 				return new int[0]; // vrati prazan niz
 
-			NapraviSito( max );
+			NapraviNizCijelihBrojeva( max );
 
-			Prosijaj();
+			EliminirajVišekratnike();
 			return PokupiPrimove();
 
 		}
@@ -66,7 +66,7 @@ namespace Vsite.Pood
 			return primovi; // vrati niz brojeva
 		}
 
-		private static void Prosijaj()
+		private static void EliminirajVišekratnike()
 		{
 			// sito (ide do kvadratnog korijena maksimalnog broja)
 
@@ -74,13 +74,18 @@ namespace Vsite.Pood
 			{
 				if ( f[i] ) // ako je i prekrižen, prekriži i višekratnike
 				{
-					for ( int j = 2 * i; j < s; j += i )
-						f[j] = false; // višekratnik nije primbroj
+					EliminirajVišekratnike( i );
 				}
 			}
 		}
 
-		private static void NapraviSito(int max)
+		private static void EliminirajVišekratnike(int i)
+		{
+			for ( int j = 2 * i; j < s; j += i )
+				f[j] = false; // višekratnik nije primbroj
+		}
+
+		private static void NapraviNizCijelihBrojeva(int max)
 		{
 			// deklaracije
 			s = max + 1; // duljina niza
