dc55dbb152de0f7aabbfe6e5740f0aaee2ce27ef
Julijan Sribar <julijan.sribar@gmail.com>
2020-11-09 19:02:33 +0100

    Skip processing first 2 numbers in the array

--- Primbrojevi/Program.cs
+++ Primbrojevi/Program.cs
@@ -48,7 +48,7 @@ namespace Vsite.Pood
         {
             // koliko je primbrojeva?
             int broj = 0;
-            for (int i = 0; i < s; ++i)
+            for (int i = 2; i < s; ++i)
             {
                 if (!jeLiEliminiran[i])
                     ++broj;
@@ -57,7 +57,7 @@ namespace Vsite.Pood
             primovi = new int[broj];
 
             // prebaci primbrojeve u rezultat
-            for (int i = 0, j = 0; i < s; ++i)
+            for (int i = 2, j = 0; i < s; ++i)
             {
                 if (!jeLiEliminiran[i])
                     primovi[j++] = i;
@@ -87,13 +87,6 @@ namespace Vsite.Pood
         {
             s = max + 1; // duljina niza
             jeLiEliminiran = new bool[s]; // niz s primbrojevima
-
-            // inicijaliziramo sve na false
-            for (int i = 0; i < s; ++i)
-                jeLiEliminiran[i] = false;
-
-            // ukloni 0 i 1 koji su primbrojevi po definiciji
-            jeLiEliminiran[0] = jeLiEliminiran[1] = true;
         }
     }
 }
